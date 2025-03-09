877b45924ae35f7e7960db067d35fae0d89a4589
kenshir0 <151419028+kenshirosalt@users.noreply.github.com>
2025-03-09 12:01:29 +0700

    dededede

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
+
+blog_posts = [
+                {"pid": 0,
+                "title": "efee",
+
+                "content": "placeholdder"
+                },
+                {"pid": 1,
+                "title": "efeee",
+                "author": "ken",
+                "content": "plaaceholdder"
+                }
+            ]
+
 def hello_world():
     return render_template("index.html")
+
+
 def cat_page():
-    return "<h1>this is a cat</h1>"
+    value = request.args.get('pid')
+    if value == None:
+        #localhost:5000/blog
+        return render_template("show_posts.html", blog_posts=blog_posts)
+    else:
+        #localhost:5000/blog?pid=0
+        #localhost:5000/blog?pid=1
+        #...
+        pid = int(value)
+        for p in blog_posts:
+            if p["pid"] == pid:
+                return render_template("posts.html", blog_data=p)
+    return render_template("show_posts.html", blog_posts=blog_posts)
 
 def posts():
     return render_template("posts.html")
@@ -24,7 +52,7 @@ def login():
 app = Flask(__name__, template_folder=os.getcwd(), static_folder=os.getcwd())
 
 app.add_url_rule("/home", "hello_world", hello_world)
-app.add_url_rule("/cat", "cat", cat_page)
+app.add_url_rule("/blog", "blog", cat_page)
 app.add_url_rule("/posts", "posts", posts)
 app.add_url_rule("/login", "login", login)
 app.add_url_rule("/createacc", "createacc", createacc)
