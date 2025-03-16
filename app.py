7daa957a8e73b0df92d018c702e4010f34c21c82
kenshir0 <151419028+kenshirosalt@users.noreply.github.com>
2025-03-16 12:07:05 +0700

    trhtrhtrheth

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import os
+
+
+https://www.reddit.com/r/reddeadredemption/comments/1gfu7ws/i_have_found_a_solution_for_access_violation_for/
+
+users = [{"uid": 0, "name": tim, "password": passw}]
 
 blog_posts = [
                 {"pid": 0,
@@ -34,10 +39,18 @@ def cat_page():
     return render_template("show_posts.html", blog_posts=blog_posts)
 
 def posts():
+
     return render_template("posts.html")
 
 def createposts():
-    return render_template("createposts.html")
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        blog_post = {"pid": len(blog_posts), "content": content, "title": title }
+        blog_posts.append(blog_post)
+        return redirect("/blog?pid=" + str(blog_post["pid"]))
+    else:
+        return render_template("createposts.html")
 
 def createacc():
     return render_template("createacc.html")
@@ -56,7 +69,7 @@ app.add_url_rule("/blog", "blog", cat_page)
 app.add_url_rule("/posts", "posts", posts)
 app.add_url_rule("/login", "login", login)
 app.add_url_rule("/createacc", "createacc", createacc)
-app.add_url_rule("/createposts", "createpost", createposts)
+app.add_url_rule("/createposts", "createpost", createposts, methods=["GET", "POST"])
 
 
 app.run()
